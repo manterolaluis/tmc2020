83ea7d5ddc44ea413ed08b0e98951a8dad8591c2
Virginia <virgi.landivar@gmail.com>
2020-04-20 15:15:20 -0300

    Acá vaa...

--- TP1/parcialito.py
+++ TP1/parcialito.py
@@ -1,23 +1,3 @@
-#Mostrar mediante un programa en python que la expresión P si y solo si -P es una contradicción
-
-valores = [False, True]
-
-# mediante reglas p si y solo si not p se puede transformar en
-# (not p or not p) and ( p or p)
-# lo que equivale a not p and p
-
-for p in valores:
-    if (not p or not p) and (p or p):
-        print("Este mensaje nunca lo verás")
-
-#alternativamente
-
-for p in valores:
-    if not p and p:
-        print("Este mensaje nunca lo verás")
-
-#alternativamente
-
-for p in valores:
-    if p == (not p):
-        print("Este mensaje nunca lo verás")
+# ejercicio de  Matemática (Vir)
+#ejercicio de Programacion (Mante)
+print ("probando pushear")

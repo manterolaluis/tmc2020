c50bc41a4bf23e07341194a94630fa563c706cf5
mante <manterolaluis@gmail.com>
2020-04-23 11:08:01 -0300

    parte de programacion del primer parcialito (logica)

--- TP1/parcialito.py
+++ TP1/parcialito.py
@@ -1,3 +1,12 @@
 # ejercicio de  Matemática (Vir)
-#ejercicio de Programacion (Mante)
-print ("probando pushear")
+# ejercicio de Programacion (Mante)
+
+################################# PROGRAMACION ##########################################################
+# Se provee el encabezado de una función python con 3 parámetros (x, y, z) que son números enteros.
+# Se deben resolver los siguientes incisos de manera independiente, pero siempre dentro del cuerpo de la función
+# - Mostrar "a es el mayor" si a es el mayor de los 3 números
+# - Mostrar "b está en el medio" si a y c son iguales y además b es mayor que a pero menor que c
+# - Mostrar "c no es el mayor" si c no es el mayor de los 3 números
+
+def funcMensajes(a, b, c):
+    #insertar código aquí
